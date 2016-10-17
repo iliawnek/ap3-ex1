94d80461510b0b13d54484d1f1512991cf9e2096
Ken Li <iliawnek@gmail.com>
2016-10-17 22:46:53 +0100

    Add comments

--- mentry.c
+++ mentry.c
@@ -7,9 +7,10 @@
 #define LINES_PER_ENTRY 3
 
 // todo: source 2
+// Copies only non-space characters from source to destination.
+// source is expected to end in \0.
+// destination is expected to be large enough to hold source.
 void delete_spaces(char source[], char destination[]) {
-    // source is supposed to be zero ended
-    // destination is supposed to be large enough to hold source
     int d = 0;
     for (int s = 0; source[s] != '\0'; s++)
         if (source[s] != ' ') {
@@ -19,12 +20,15 @@ void delete_spaces(char source[], char destination[]) {
     destination[d] = '\0';
 }
 
+// Converts every char in source to lower.
 void string_to_lower(char source[]) {
     for (int s = 0; source[s] != '\0'; s++) {
         source[s] = (char) tolower(source[s]);
     }
 }
 
+// Returns the next MEntry from fd.
+// Returns null if EOF is reached.
 MEntry *me_get(FILE *fd) {
     // allocate space in memory for new MEntry
     MEntry *me = malloc(sizeof(MEntry));
@@ -40,16 +44,22 @@ MEntry *me_get(FILE *fd) {
             // todo: deallocate current MEntry from memory
             return NULL;
         }
+
+        // handle name line
         if (i == 0) {
             surname = malloc(strlen(buffer) * sizeof(char));
             sscanf(buffer, "%[^,]", surname); // todo: source 1
             string_to_lower(surname);
             printf("%s\n", surname);
-        } else if (i == 1) {
+        }
+        // handle address line
+        else if (i == 1) {
             if (sscanf(buffer, "%d", house_number) != 0) {
                 printf("%d\n", *house_number);
             }
-        } else if (i == 2) {
+        }
+        // handle postcode line
+        else if (i == 2) {
             postcode = malloc(strlen(buffer) * sizeof(char));
             delete_spaces(buffer, postcode);
             string_to_lower(postcode);
