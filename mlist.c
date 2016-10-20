f07e3273ffa70ff1390fef5e5557e01dac95263f
Ken Li <iliawnek@gmail.com>
2016-10-20 21:04:19 +0100

    Reimplement mlist.c using separate chaining with linked lists

--- mlist.c
+++ mlist.c
@@ -2,13 +2,23 @@
 #include "mentry.h"
 #include <stdlib.h>
 
-#define INITIAL_HASH_TABLE_SIZE 40 // double the specified 20
+#define BUCKET_CAPACITY 20 // hash table resizes if any bucket exceeds this size
+#define INITIAL_HASH_TABLE_SIZE 100
 
-MList *expand_hash_table(MList **ml);
+void expand_hash_table(MList **ml);
+
+typedef struct bucketitem {
+    MEntry *me;
+    struct bucketitem *next;
+} BucketItem;
+
+typedef struct bucket {
+    BucketItem *first;
+    int size;
+} Bucket;
 
 struct mlist {
-    int buckets_filled;
-    MEntry **hash_table; // todo: pointer to a pointer? pointer to an array?
+    Bucket **hash_table; // todo: pointer to a pointer? pointer to an array?
 };
 
 unsigned int hash_table_size = INITIAL_HASH_TABLE_SIZE;
@@ -18,10 +28,9 @@ int ml_verbose = 0;
 MList *ml_create(void) {
     if (ml_verbose) fprintf(stderr, "ml_create()\n");
     MList *ml = malloc(sizeof(MList));
-    if (ml == NULL) return NULL; // no space in memory for mailing list
-    ml->buckets_filled = 0;
-    ml->hash_table = calloc(hash_table_size, sizeof(MEntry));
-    if (ml->hash_table == NULL) return NULL; // no space in memory for mailing list
+    if (ml == NULL) return NULL; // no space in memory for ew mailing list
+    ml->hash_table = calloc(hash_table_size, sizeof(Bucket));
+    if (ml->hash_table == NULL) return NULL; // no space in memory for new mailing list
     return ml;
 }
 
@@ -31,24 +40,43 @@ MList *ml_create(void) {
 // Returns 0 if error (malloc).
 int ml_add(MList **ml, MEntry *me) {
     if (ml_verbose) fprintf(stderr, "ml_add()\n");
-    if ((*ml)->buckets_filled == (hash_table_size / 2)) expand_hash_table(ml);
+
+    Bucket **hash_table = (*ml)->hash_table;
     unsigned int index = me_hash(me, hash_table_size);
-    MEntry **hash_table = (*ml)->hash_table;
-    MEntry *cursor;
-    while (1) {
-        cursor = hash_table[index];
-        // found a space
-        if (cursor == NULL) {
-            hash_table[index] = me;
-            (*ml)->buckets_filled++;
-            return 1;
-        }
-        // trying to add duplicate
-        if (cursor == me) return 1;
-        // reached end of hash table, need to wrap round
-        if (index == hash_table_size - 1) index = 0;
-        else index++;
+    Bucket *bucket = hash_table[index];
+
+    // no bucket yet
+    if (bucket == NULL) {
+        bucket = malloc(sizeof(Bucket));
+        if (bucket == NULL) return 0; // no space in memory for new bucket
+        BucketItem *bucket_item = malloc(sizeof(BucketItem));
+        if (bucket_item == NULL) return 0;  // no space in memory for new bucket
+        bucket_item->me = me;
+        bucket_item->next = NULL;
+        bucket->first = bucket_item;
+        bucket->size = 1;
+        return 1;
     }
+
+    // bucket is full
+    if (bucket->size == BUCKET_CAPACITY) {
+        expand_hash_table(ml);
+        return ml_add(ml, me); // try adding entry to expanded hash table
+    }
+
+    // check for duplicate
+    BucketItem *cursor = bucket->first;
+    do {
+        if (me_compare(cursor->me, me) == 0) return 1;
+    } while ((cursor = cursor->next) != NULL);
+
+    // add entry to bucket
+    BucketItem *bucket_item = malloc(sizeof(BucketItem));
+    bucket_item->me = me;
+    bucket_item->next = bucket->first;
+    bucket->first = bucket_item;
+    bucket->size++; // todo: make sure this line works
+    return 1;
 }
 
 // Looks for entry in mailing list.
@@ -56,43 +84,64 @@ int ml_add(MList **ml, MEntry *me) {
 // Returns NULL if not found.
 MEntry *ml_lookup(MList *ml, MEntry *me) {
     if (ml_verbose) fprintf(stderr, "ml_lookup()\n");
+
     unsigned int index = me_hash(me, hash_table_size);
-    MEntry **hash_table = ml->hash_table;
-    MEntry *cursor;
-    while (1) {
-        cursor = hash_table[index];
-        if (cursor == NULL) return NULL;
-        if (me_compare(cursor, me) == 0) return cursor;
-        if (index == hash_table_size - 1) index = 0;
-        else index++;
-    }
+    Bucket **hash_table = ml->hash_table;
+    Bucket *bucket = hash_table[index];
+
+    if (bucket == NULL) return NULL; // bucket doesn't exist, therefore no entry
+
+    BucketItem *cursor = bucket->first;
+    do {
+        if (me_compare(cursor->me, me) == 0) return me;
+    } while ((cursor = cursor->next) != NULL);
+    return NULL; // bucket exists, but entry not found
 }
 
 // Destroys mailing list.
 void ml_destroy(MList *ml) {
     if (ml_verbose) fprintf(stderr, "ml_destroy()\n");
-    MEntry **hash_table = ml->hash_table;
-    MEntry *cursor;
-    for (int i = 0; i < hash_table_size; i++) {
-        cursor = hash_table[i];
-        if (cursor != NULL) free(cursor);
-    };
-    free(ml);
-}
 
-MList *expand_hash_table(MList **ml) {
-    if (ml_verbose) fprintf(stderr, "expand_hash_table()\n");
-    hash_table_size *= 2;
-    MList *new_ml = ml_create();
-    MEntry **hash_table = (*ml)->hash_table;
-    MEntry *cursor;
+    Bucket **hash_table = ml->hash_table;
+    Bucket *bucket;
+    BucketItem *bucket_item;
+    BucketItem *next_bucket_item;
     for (int i = 0; i < hash_table_size; i++) {
-        cursor = hash_table[i];
-        if (cursor != NULL) {
-            ml_add(&new_ml, cursor);
-            free(cursor);
+        bucket = hash_table[i];
+        if (bucket != NULL) {
+            bucket_item = bucket->first;
+            do {
+                free(bucket_item->me);
+                next_bucket_item = bucket_item->next;
+                free(bucket_item);
+            } while ((bucket_item = next_bucket_item) != NULL);
+            free(bucket);
         }
     }
     free(ml);
-    return new_ml;
+}
+
+void expand_hash_table(MList **ml) {
+    if (ml_verbose) fprintf(stderr, "expand_hash_table()\n");
+
+    hash_table_size *= 2;
+    MList *new_ml = ml_create();
+    Bucket **hash_table = (*ml)->hash_table;
+    Bucket *bucket;
+    BucketItem *bucket_item;
+    BucketItem *next_bucket_item;
+    for (int i = 0; i < hash_table_size; i++) {
+        bucket = hash_table[i];
+        if (bucket != NULL) {
+            bucket_item = bucket->first;
+            do {
+                ml_add(&new_ml, bucket_item->me);
+                next_bucket_item = bucket_item->next;
+                free(bucket_item);
+            } while ((bucket_item = next_bucket_item) != NULL);
+            free(bucket);
+        }
+    }
+    free(*ml);
+    *ml = new_ml;
 }
